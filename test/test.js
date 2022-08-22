ae68675ea2014d97feb81e994e1744f687e38adb
Hardik Parmar <hardik.parmar5@gmail.com>
2022-08-22 16:52:13 +0530

    update and find by id api testing using chai js

--- test/test.js
+++ test/test.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 const products = require("../model/productModel");
-const request = require("supertest");
+// const request = require("supertest");
 
 //require dependencies
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const { expect } = require("chai");
 const app = require("../index");
-
+let should = chai.should();
 chai.use(chaiHttp);
 
 //it will remove empty products
@@ -79,20 +79,63 @@ describe("/POST products", () => {
 
 // product by id test
 
-// describe("/POST products", () => {
-//   it("it should get the product by id", (done) => {
-//     let ProductId = chai
-//       .request(app)
-//       .post("/search/:id")
-//       .send(product)
-//       .end((err, res) => {
-//         expect(res).to.have.status(200);
-//         expect(res.body).to.be.a("object");
-//         expect(res.body).to.have.property("productName");
-//         expect(res.body).to.have.property("productCatagory");
-//         expect(res.body).to.have.property("productPrice");
-//         expect(res.body).to.have.property("productQuantity");
-//         done();
-//       });
-//   });
-// });
+describe("/GET products/search/:id", () => {
+  it("it should get the product by id", (done) => {
+    let product = new products({
+      productName: "Laptop698",
+      productCatagory: "Electronics21",
+      productPrice: 450200,
+      productQuantity: 21239,
+    });
+    product.save((err, product) => {
+      chai
+        .request(app)
+        .get("products/search" + product.id)
+        .send(product)
+        .end((err, res) => {
+          expect(200, done);
+          expect(product).to.be.a("object");
+          expect(product).to.have.property("productName");
+          expect(product).to.have.property("productCatagory");
+          expect(product).to.have.property("productPrice");
+          expect(product).to.have.property("productQuantity");
+          expect(product).to.have.property("_id");
+          done();
+        });
+    });
+  });
+});
+
+// update by id
+
+describe("/PUT/:id ", () => {
+  it("it should update a product using  the id", (done) => {
+    let product = new products({
+      productName: "Laptop698",
+      productCatagory: "Electronics21",
+      productPrice: 450200,
+      productQuantity: 21239,
+    });
+    product.save((err, product) => {
+      chai
+        .request(app)
+        .put("/products/update/" + product.id)
+        .send({
+          productName: "mobile",
+          productCatagory: "Electronics221",
+          productPrice: 450,
+          productQuantity: 29,
+        })
+        .end((err, res) => {
+          expect(200, done);
+          expect(product).to.be.a("object");
+          expect(product).to.have.property("productName");
+          expect(product).to.have.property("productCatagory");
+          expect(product).to.have.property("productPrice");
+          expect(product).to.have.property("productQuantity").eql(21239);
+          expect(product).to.have.property("_id");
+          done();
+        });
+    });
+  });
+});
